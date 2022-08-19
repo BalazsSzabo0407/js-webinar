4dbf5fa892eac1c0fed1606f2040a8f0a1a2a872
Balazs Szabo <szabo.balazs0407@gmail.com>
2022-08-19 11:11:24 +0200

    Solution for module 1

--- module-2/test/calc.spec.js
+++ module-2/test/calc.spec.js
@@ -18,4 +18,50 @@ describe.only('calc', () => {
      */
     // TODO: write test cases to test the calculator
 
+    it("should have proper value");
+
+    describe("add", () => { 
+        it("should exist");
+
+        it("should be able to add number to current value");
+    });
+    describe("minus", () => {
+        it("should exist");
+
+        it("should be able to substract a number from the current value");
+    });
+    // ...
+    describe("divide", () => {
+        it("should exist", () => {
+            //Given
+            const c = calc(42)
+            //When
+            //Then
+            expect(c.divide).not.to.be.undefined;
+        });
+
+        it("should be able to perform a valid division", () => {
+            // Given
+            const c = calc(10);
+            // When
+            const result = c.divide(2).v;
+            // Then
+            expect(result).to.equal(5);
+        });
+
+        it("should handle division by 0", () => {
+            //given
+            const c = calc(42);
+            //when 
+            //then
+            expect(() => c.divide.bind(0)).to.throw("Division");
+            expect(c.divide.bind(null,0)).to.throw("Division");
+        });
+    });
+    // ...
+
+
+
+
+
 });
